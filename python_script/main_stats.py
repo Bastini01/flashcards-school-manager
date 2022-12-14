72f024504a51511db5029dfd1f1239d5a60a8acb
Bastini01 <pierrehenry.bastin@gmail.com>
2022-12-14 18:07:53 +0800

    stats correction

--- python_script/main_stats.py
+++ python_script/main_stats.py
@@ -405,11 +405,13 @@ def line_follow_date(lineLog):
         return x[0] if len(x)>0 else None
     df = lineLog['full'].apply(line_event)
     df = df[df.notnull()]
-    df = df.loc[lambda x: x['type'] == 'follow']
+    # df = df.loc[lambda x: x['type'] == 'follow']
     df1 = pd.DataFrame()
     df1['type'] = df.apply(lambda x: x['type'])
     df1['regDate'] = df.apply(lambda x: dt.datetime.fromtimestamp(x['timestamp']/1000).date())
-    df1['Line ID'] = df.apply(lambda x: x['source']['userId'])
+    df1['source'] = df.apply(lambda x: x['source'])
+    df1 = df1[df1['type'] == 'follow']
+    df1['Line ID'] = df1.apply(lambda x: x['source']['userId'])
     # df1 = df1[df1['type'] == 'follow']
     df1 = df1.groupby('Line ID', as_index=False).agg({'regDate':'min'})
     return df1
